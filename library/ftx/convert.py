2f91990c4b8641e4ac56ae542f040d63b5655971
danyanyam <dvbuchko@edu.hse.ru>
2021-08-26 00:26:59 +0300

    added new modules

---
+++ library/ftx/convert.py
@@ -0,0 +1,20 @@
+from library.ftx.base import BaseApiClass
+
+
+class Convert(BaseApiClass):
+    """docstring for Fills."""
+
+    def __init__(self, api_key: str, secret_key: str, subaccount_name: str = ''):
+        super().__init__(api_key, secret_key, subaccount_name)
+
+    async def request_quote(self, fromCoin: str, toCoin: str, size: float):
+        """ https://docs.ftx.com/#request-quote """
+        return self.post('/api/otc/quotes', data={'fromCoin': fromCoin, 'toCoin': toCoin, 'size': size})
+
+    async def get_quote_status(self, quoteId: str, market: str = ''):
+        """ https://docs.ftx.com/#get-quote-status """
+        return self.get(f'/api/quotes/{quoteId}', market=market)
+
+    async def accept_quote(self, quoteId: int):
+        """ https://docs.ftx.com/#accept-quote """
+        return self.post(f'/api/otc/quotes/{quoteId}/accept')
---
+++ library/ftx/latency_statistics.py
@@ -0,0 +1,12 @@
+from library.ftx.base import BaseApiClass
+
+
+class LatencyStatistics(BaseApiClass):
+    """docstring for Fills."""
+
+    def __init__(self, api_key: str, secret_key: str, subaccount_name: str = ''):
+        super().__init__(api_key, secret_key, subaccount_name)
+
+    async def get(self, days: int = None, subaccount_nickname: str = None):
+        """ https://docs.ftx.com/#latency-statistics """
+        return self.get('/api/stats/latency_stats', days=days, subaccount_nickname=subaccount_nickname)
