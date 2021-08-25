a93538911a4335068406b4fc5a023b1e0756a0ea
danyanyam <dvbuchko@edu.hse.ru>
2021-08-25 17:36:13 +0300

    some fixes

--- library/ftx/futures.py
+++ library/ftx/futures.py
@@ -11,24 +11,24 @@ class Futures(ApiObject):
 
     async def list_all_futures(self):
         """ https://docs.ftx.com/#futures """
-        return await self.get('/futures', authentication_required=False)
+        return await self.get('/api/futures', authentication_required=False)
 
     async def get_future(self, future_name: str):
         """ https://docs.ftx.com/#get_future """
-        return await self.get(f'/futures/{future_name}', authentication_required=False)
+        return await self.get(f'/api/futures/{future_name}', authentication_required=False)
 
     async def get_future_stats(self, future_name: str):
         """ https://docs.ftx.com/#get_future """
-        return await self.get(f'/futures/{future_name}/stats', authentication_required=False)
+        return await self.get(f'/api/futures/{future_name}/stats', authentication_required=False)
 
     async def get_funding_rates(self, start_time: dt.datetime = None, end_time: dt.datetime = None):
         """ https://docs.ftx.com/#get-funding-rates """
-        return await self.get(f'/funding_rates', authentication_required=False, start_time=start_time, end_time=end_time)
+        return await self.get(f'/api/funding_rates', authentication_required=False, start_time=start_time, end_time=end_time)
 
     async def get_index_weights(self, index_name: str):
         """ https://docs.ftx.com/#get-index-weights """
-        return await self.get(f'/indexes/{index_name}/weights', authentication_required=False)
+        return await self.get(f'/api/indexes/{index_name}/weights', authentication_required=False)
 
     async def get_expired_futures(self):
         """ https://docs.ftx.com/#get-index-weights """
-        return await self.get(f'/expired_futures', authentication_required=False)
+        return await self.get(f'/api/expired_futures', authentication_required=False)
--- library/ftx/markets.py
+++ library/ftx/markets.py
@@ -10,16 +10,16 @@ class Markets(ApiObject):
 
     async def get_markets(self):
         """ https://docs.ftx.com/#markets """
-        return await self.get('/markets', authentication_required=False)
+        return await self.get('/api/markets', authentication_required=False)
 
     async def get_single_market(self, market_name: str):
         """ https://docs.ftx.com/#get-single-market """
-        return await self.get(f'/markets/{market_name}', authentication_required=False)
+        return await self.get(f'/api/markets/{market_name}', authentication_required=False)
 
     async def get_orderbook(self, market_name: str, depth: float):
         """ https://docs.ftx.com/#get-orderbook """
-        return await self.get(f'/markets/{market_name}/orderbook?depth={depth}', authentication_required=False)
+        return await self.get(f'/api/markets/{market_name}/orderbook?depth={depth}', authentication_required=False)
 
     async def get_trades(self, market_name: str, start_time: dt.datetime = None, end_time: dt.datetime = None):
         """ https://docs.ftx.com/#get-trades """
-        return await self.get(f'/markets/{market_name}/trades', authentication_required=False, start_time=start_time, end_time=end_time)
+        return await self.get(f'/api/markets/{market_name}/trades', authentication_required=False, start_time=start_time, end_time=end_time)
